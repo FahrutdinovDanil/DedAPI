635ddf5e6b71ff0cfd586e967cf1c9d54e39818f
Даниль Фахрутдинов <danilfahrutdinov357@mail.ru>
2022-05-11 22:28:28 +0300

    Добавьте файлы проекта.

---
+++ DedAPI/DedAPI/Views/EntrieListPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using DedAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DedAPI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntrieListPage : ContentPage
+    {
+        public List<EntryModel> Entries { get; set; }
+        public EntrieListPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            Entries = await App.RequestManager.GetEntrieModels();
+            LVEntries.ItemsSource = Entries;
+        }
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var searchText = SearchBar.Text.ToLower();
+
+
+            LVEntries.ItemsSource = Entries.Where(en => en.API.ToLower().Contains(searchText) || en.Description.ToLower().Contains(searchText));
+        }
+
+        private async void LVEntries_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var entry = LVEntries.SelectedItem as EntryModel;
+
+            await Navigation.PushAsync(new EntryPage(entry));
+        }
+    }
+}
---
+++ DedAPI/DedAPI/Views/EntryPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using DedAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DedAPI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntryPage : ContentPage
+    {
+        public EntryModel Entry { get; set; }
+        public EntryPage(EntryModel entryModel)
+        {
+            InitializeComponent();
+            Entry = entryModel;
+            this.BindingContext = this;
+            LBHTTPS.Text = Entry.HTTPS ? "Поддерживает HTTPS" : "Не поддерживает HTTPS";
+        }
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Uri uri = new Uri(Entry.Link);
+
+            await Browser.OpenAsync(uri);
+        }
+    }
+}
